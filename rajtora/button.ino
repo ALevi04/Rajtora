7b4ee0bda38ee4c897705c342786359adc1f505c
ALevi04 <59363970+ALevi04@users.noreply.github.com>
2020-10-18 00:10:23 +0200

    1.1
    Button perform reformation

---
+++ rajtora/button.ino
@@ -0,0 +1,74 @@
+int const buttons[] = {10, 11, 12};
+long pressed[] = {0, 0, 0};
+bool clicked[] = {false, false, false};
+
+void buttonInit() {
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+}
+void buttonPerform() {
+  for (int i = 0; i < 3; i++) {
+    int port = buttons[i];
+    long pressedAt = pressed[i];
+    bool isClicked = clicked[i];
+    if (stateRead(port) && pressedAt != 0 && !isClicked) {
+      if (millis() - pressedAt > 750) {
+        Serial.println("long");
+        //press & hold actions
+        switch (port) {
+          case 10: changeBright(); break;
+          case 11: changeColor(); break;
+          case 12: break;
+        }
+        pressedAt = 0;
+        isClicked = true;
+      }
+    }
+    else if (!stateRead(port) && pressedAt != 0 && !isClicked) {
+      Serial.println("short");
+      //click action
+      switch (port) {
+        case 10: timePlus(); break;
+        case 11: timeMinus(); break;
+        case 12: startStop(); break;
+      }
+      pressedAt = 0;
+    }
+    else if (!stateRead(port) && isClicked) {
+      pressedAt = 0;
+      isClicked = false;
+    }
+    else if (stateRead(port) && pressedAt == 0) {
+      pressedAt = millis();
+    }
+
+    pressed[i] = pressedAt ;
+    clicked[i] = isClicked;
+  }
+}
+bool stateRead(int port) {
+  return !digitalRead(port);
+}
+void timePlus() {
+  time = time + 100;
+}
+void timeMinus() {
+  time = time - 100;
+}
+void changeColor() {
+  c++;
+  if (c == 3) {
+    c == 0;
+  }
+}
+void changeBright() {
+  if (bright == 100) {
+    bright = 0;
+  }
+  bright = bright + 25;
+  displayBright(bright);
+}
+void startStop() {
+  started = !started;
+}
